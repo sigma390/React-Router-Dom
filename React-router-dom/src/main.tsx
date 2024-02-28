e2fda669fecd295678210c4d8eb96384d735eec4
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-28 21:47:40 +0530

    Router Created !!!

--- React-router-dom/src/main.tsx
+++ React-router-dom/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider } from 'react-router/dist/lib/components'
+import { createBrowserRouter } from 'react-router-dom'
+
+
+const router = createBrowserRouter([])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>,
 )
