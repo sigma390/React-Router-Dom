97e15c203532eabdcc9b28f5fb34171543559748
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-28 22:25:22 +0530

    Created Router and Added Parent and Children Path

--- React-router-dom/src/main.tsx
+++ React-router-dom/src/main.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
-import { RouterProvider } from 'react-router/dist/lib/components'
+import { RouterProvider } from 'react-router-dom'
 import { createBrowserRouter } from 'react-router-dom'
+import { Layout } from './Layout.tsx'
+import { About } from './Components/About/About.tsx'
+import Home from './Components/Home/Home.tsx'
 
 
-const router = createBrowserRouter([])
+const router = createBrowserRouter([{
+  path:'/',
+  element:<Layout/>, //parent element
+  children:[{
+    path:'about',
+    element:<About/>
+
+  },{
+    path:'',
+    element:<Home/>
+
+  }]
+
+}])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
