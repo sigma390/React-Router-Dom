e67cd1fc3c702e8813bdf1eb5a7ef2f06f154889
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-29 11:04:07 +0530

    Github Call made!!!

--- React-router-dom/src/Components/Github/Github.tsx
+++ React-router-dom/src/Components/Github/Github.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const url = 'https://api.github.com/users/sigma390'
+
+
 
 export const Github = () => {
+   
+   // Assuming data is an object
+    const [followersCount, setFollowersCount] = useState(0); // Initialize followers count to 0
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(url);
+       
+          // Extract followers count from the response and update state
+          setFollowersCount(response.data.followers);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData(); // Call the fetchData function when the component mounts
+    }, []); // Empty dependency array ensures useEffect runs only once when component mounts
   return (
-    <div>Github</div>
+    <div className='text-center
+     m-4 bg-gray-600
+      text-white'>Github Followers:{followersCount}</div>
   )
 }
